565418ae828df00a3580f6da568804416b56d249
tuanbin298 <tuannase171419@fpt.edu.vn>
2024-09-24 21:39:34 +0700

    Update

--- koi_fe/src/App.jsx
+++ koi_fe/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Homepage from './page/HomePage/Homepage'
+import { useState } from "react";
+import Homepage from "./page/HomePage/Homepage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <Homepage/>
+      <Homepage />
     </>
-  )
+  );
 }
 
 export default App;
