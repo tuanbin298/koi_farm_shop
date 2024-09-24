4734d6df1d50926e79147a893c7f18cba6626cb1
Khanh Hang <hangntkse170180@fpt.edu.vn>
2024-09-24 18:28:08 +0700

    “update”

--- koi_fe/src/page/LoginPage/LoginPage.jsx
+++ koi_fe/src/page/LoginPage/LoginPage.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './LoginPage.css';
 
-export default function LoginPage() {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="login-container">
+      <h2 className="login-title">Đăng nhập</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Mật khẩu"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit" className="login-button">Đăng nhập</button>
+      </form>
+      <a href="/forgot-password" className="forgot-password">Quên mật khẩu?</a>
+      <div className="register-link-container">
+      <p className="login-redirect">Bạn chưa có tài khoản đăng ký <a href="/login">Tại đây</a></p>
+
+      </div>
+    </div>
+  );
+};
+
+export default Login;
