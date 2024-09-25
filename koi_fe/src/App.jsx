fbee585c7f58e28c7e6d7a7fdf6893deb05b8068
NguyenDang38 <dangnpmse184193@fpt.edu.vn>
2024-09-25 21:24:00 +0700

    added Browser router

--- koi_fe/src/App.jsx
+++ koi_fe/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 import Homepage from "./page/HomePage/Homepage";
-
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./page/LoginPage/LoginPage";
+import RegisterPage from "./page/RegisterPage/RegisterPage";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Homepage />
+    
+    <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="register" element={<RegisterPage/>}/>
+      </Routes>
+    </BrowserRouter>
+      <Footer/>
     </>
   );
 }
