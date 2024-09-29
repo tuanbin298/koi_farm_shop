32e9f176a27543b6abc1aeb9bd62af7aa46eb855
Khanh Hang <hangntkse170180@fpt.edu.vn>
2024-09-29 15:20:08 +0700

    add article schema

--- koi_be/schema/index.ts
+++ koi_be/schema/index.ts
@@ -2,10 +2,12 @@ import User from "./User.schema";
 import Role from "./Role.schema";
 import Product from "./Product.schema";
 import Category from "./Category.schema";
+import Article from "./Article.schema";
 
 export const lists = {
   User,
   Role,
   Product,
   Category,
+  Article,
 };
