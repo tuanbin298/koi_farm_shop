793595d0c6428087d4e08fbe34608129eff802cd
Khanh Hang <hangntkse170180@fpt.edu.vn>
2024-10-04 23:42:24 +0700

    profileuser

--- koi_fe/src/page/ProfileUser/ProfileUser.jsx
+++ koi_fe/src/page/ProfileUser/ProfileUser.jsx
@@ -28,10 +28,10 @@ const ProfileUser = () => {
 
       <div className="profile-right">
         <h3>TÀI KHOẢN CỦA TÔI</h3>
-        <p><strong>Tên tài khoản:{localStorage.getItem("name")}</strong></p>
-        <p><strong>Email:{localStorage.getItem("email")}</strong> </p>
-        <p><strong>Điện thoại:{localStorage.getItem("phone")}</strong> </p>
-        <p><strong>Địa chỉ:{localStorage.getItem("address")}</strong> </p>
+        <p><strong>Tên tài khoản: {localStorage.getItem("name")}</strong></p>
+        <p><strong>Email: {localStorage.getItem("email")}</strong> </p>
+        <p><strong>Điện thoại: {localStorage.getItem("phone")}</strong> </p>
+        <p><strong>Địa chỉ: {localStorage.getItem("address")}</strong> </p>
         <button className="address-btn">Sửa địa chỉ </button>
       </div>
     </div>
