9017d01a572082fe91c76de11085fcf90ac56489
NguyenDang38 <dangnpmse184193@fpt.edu.vn>
2024-10-14 11:04:07 +0700

    show cart items in cartPage

---
+++ koi_fe/src/page/api/Queries/cartItem.ts
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+// Get all data of category
+export const GET_CART_ITEMS = gql`
+  query Query($where: CartItemWhereInput!) {
+  cartItems(where: $where) {
+    product {
+      image {
+        publicUrl
+      }
+      name
+      price
+    }
+  }
+}
+`;
