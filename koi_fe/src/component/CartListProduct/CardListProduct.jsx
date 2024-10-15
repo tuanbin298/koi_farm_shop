67ec5aed67fc71b4545c70d301b95f5e68d13ce6
NguyenDang38 <dangnpmse184193@fpt.edu.vn>
2024-10-15 22:21:55 +0700

    update handleAddToCart for product list in home page and Koi list page

--- koi_fe/src/component/CartListProduct/CardListProduct.jsx
+++ koi_fe/src/component/CartListProduct/CardListProduct.jsx
@@ -1,14 +1,68 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { formatMoney } from "../../utils/formatMoney";
+import { CREATE_CART_ITEM } from "../../page/api/Mutations/cart";
+import toast, { Toaster } from "react-hot-toast";
+import { useMutation } from "@apollo/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function CardListProduct({ products }) {
+  const [createCartItem] = useMutation(CREATE_CART_ITEM);
   if (!products || products.length === 0) {
     return <p>Không có sản phẩm nào phù hợp.</p>;
   }
+  const handleAddToCart = async (productId) => {
+    const userId = localStorage.getItem("id"); // Assuming userId is stored in localStorage
+    const sessionToken = localStorage.getItem("sessionToken");
+
+    if (!userId) {
+      alert("User ID not found. Please log in.");
+      return;
+    }
+
+    try {
+      // Add item to the cart
+      await createCartItem({
+        variables: {
+          data: {
+            quantity: 1,
+            product: {
+              connect: { id: productId }, // Connect product by ID
+            },
+            user: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        },
+      });
+      toast.success("Đã thêm vào giỏ hàng!", {
+        icon: "🛒",
+        style: {
+          border: "1px solid #713200",
+          padding: "16px",
+          color: "#982B1C",
+        },
+        iconTheme: {
+          primary: "#982B1C",
+          secondary: "#FFFAEE",
+        },
+      });
+    } catch (error) {
+      if (error.message.includes("Access denied")) {
+        toast.error("Thêm vào giỏ hàng không thành công");
+        console.log(error);
+      } else {
+        toast.error("Thêm vào giỏ hàng không thành công");
+        alert("Failed to add item to cart. Please try again.");
+      }
+    }
+  };
 
   return (
+    <>
+    <Toaster position="top-center" reverseOrder={false} />
     <div className="container mt-4 species-section">
       <div className="row">
         {products.map((product) => (
@@ -66,18 +120,17 @@ export default function CardListProduct({ products }) {
                   Dainichi Koi Farm
                 </p>
                 {/* Nút thêm vào giỏ hàng */}
-                <Link to={`/ProductDetail/${product.id}`}>
                   <div className="text-center">
-                    <button className="btn btn-success mt-3">
+                    <button className="btn btn-success mt-3" onClick={() => handleAddToCart(product.id)}>
                       Thêm vào giỏ hàng
                     </button>
                   </div>
-                </Link>
               </div>
             </div>
           </div>
         ))}
       </div>
     </div>
+    </>
   );
 }
