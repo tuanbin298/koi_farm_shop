7d7e52a49efa7a20e96134bc75a3b8b832108b50
tuanbin298 <tuannase171419@fpt.edu.vn>
2024-11-18 02:20:11 +0700

    Update

--- koi_be/schema/Product.schema.ts
+++ koi_be/schema/Product.schema.ts
@@ -1,7 +1,6 @@
 import { list } from "@keystone-6/core";
 import { allowAll } from "@keystone-6/core/access";
 import { text, relationship, integer, select } from "@keystone-6/core/fields";
-import { cloudinaryImage } from "@keystone-6/cloudinary";
 import { permissions } from "../auth/access";
 import "dotenv/config";
 import buildSlug from "../utils/buildSlug";
