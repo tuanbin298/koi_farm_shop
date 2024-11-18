6aad9f5c505eb1a3be8de16e01797bea6b70b7fc
tuanbin298 <tuannase171419@fpt.edu.vn>
2024-11-18 21:49:43 +0700

    Update

--- koi_be/schema/OrderItem.schema.ts
+++ koi_be/schema/OrderItem.schema.ts
@@ -58,8 +58,10 @@ const OrderItem = list({
       label: "Trạng thái",
       defaultValue: "Đang xử lý",
       options: [
-        { label: "Đang giao hàng", value: "Đang giao hàng" },
         { label: "Đang xử lý", value: "Đang xử lý" },
+        { label: "Đang chăm sóc", value: "Đang chăm sóc" },
+        { label: "Kết thúc ký gửi", value: "Kết thúc ký gửi" },
+        { label: "Đang giao hàng", value: "Đang giao hàng" },
         { label: "Hoàn thành", value: "Hoàn thành" },
       ],
     }),
