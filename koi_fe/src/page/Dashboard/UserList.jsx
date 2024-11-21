e93f2391fb1e8a0ac63144a5c3d02bbe6e3c5f62
Khanh Hang <hangntkse170180@fpt.edu.vn>
2024-11-22 01:01:10 +0700

    update

--- koi_fe/src/page/Dashboard/UserList.jsx
+++ koi_fe/src/page/Dashboard/UserList.jsx
@@ -13,33 +13,30 @@ import {
   Modal,
   TextField,
   CircularProgress,
-  Checkbox,
+  Checkbox
 } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
 import UpdateIcon from "@mui/icons-material/Update";
 import toast, { Toaster } from "react-hot-toast";
 import { useQuery, useMutation } from "@apollo/client";
 
 import { GET_PROFILE_ADMIN, GET_PROFILE } from "../api/Queries/user";
 import { UPDATE_USER } from "../api/Mutations/user";
-import { DELETE_USERS } from "../api/Mutations/user";
 
 const UserList = () => {
-  const userId = localStorage.getItem("id");
+  const userId = localStorage.getItem("id")
   // Query
   const { loading, error, data, refetch } = useQuery(GET_PROFILE_ADMIN);
-  const { data: userData } = useQuery(GET_PROFILE, {
-    variables: {
-      where: {
-        id: userId,
-      },
-    },
-  });
+  const {data:userData} = useQuery(GET_PROFILE, {
+    variables:{
+      where:{
+        id: userId
+      }
+    }
+  })
   const users = data?.users || [];
-  console.log(users);
+  console.log(users)
   // Mutation
   const [updateUser] = useMutation(UPDATE_USER);
-  const [deleteUsers] = useMutation(DELETE_USERS);
 
   // State
   const [selectedUsers, setSelectedUsers] = useState([]);
@@ -48,8 +45,6 @@ const UserList = () => {
   const [openModal, setOpenModal] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [originalUser, setOriginalUser] = useState(null);
-  const [selectedUserIds, setSelectedUserIds] = useState([]);
-  const [isSelectAll, setIsSelectAll] = useState(false);
 
   // Handle
   const handleRowClick = (user) => {
@@ -107,44 +102,11 @@ const UserList = () => {
         handleCloseModal();
       } catch (error) {
         toast.error("Lỗi cập nhật người dùng!");
-        console.error("Đã xảy ra lỗi khi cập nhật người dùng:", error);
+        console.error("Đã xảy ra lỗi khi cập nhật người dùng :", err);
       }
     } else {
       toast("Không có gì thay đổi");
       handleCloseModal();
-    }
-  };
-
-  const handleCheckboxChange = (id) => {
-    setSelectedUserIds((prevIds) =>
-      prevIds.includes(id)
-        ? prevIds.filter((userId) => userId !== id)
-        : [...prevIds, id]
-    );
-  };
-
-  const handleSelectAll = () => {
-    if (isSelectAll) {
-      setSelectedUserIds([]);
-    } else {
-      setSelectedUserIds(data.users.map((user) => user.id));
-    }
-    setIsSelectAll(!isSelectAll);
-  };
-
-  const handleDeleteSelectedUsers = async () => {
-    try {
-      const selectedUsers = selectedUserIds.map((id) => ({ id }));
-      const { data } = await deleteUsers({
-        variables: { where: selectedUsers },
-      });
-      toast.success(`Đã xóa ${data.deleteUsers.length} người dùng thành công!`);
-      setSelectedUserIds([]);
-      setIsSelectAll(false);
-      await refetch();
-    } catch (error) {
-      toast.error("Xóa người dùng thất bại!");
-      console.error(error);
     }
   };
 
@@ -162,9 +124,19 @@ const UserList = () => {
         color="error"
         sx={{ textAlign: "center", marginTop: 4 }}
       >
-        Error loading users: {error.message}
+        Error loading articles: {error.message}
       </Typography>
     );
+
+
+    // When check one checkbox
+  const handleCheckboxChange = (userId) => {
+    setSelectedUsers((prevSelected) =>
+      prevSelected.includes(userId)
+        ? prevSelected.filter((id) => id !== userId)
+        : [...prevSelected, userId]
+    );
+  };
 
   // When check all checkbox
   const handleSelectAllChange = () => {
@@ -189,212 +161,199 @@ const UserList = () => {
           borderRadius: "8px",
         }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            mb: 2,
-          }}
-        >
-          <Typography variant="h4" sx={{ mb: 3 }}>
-            Danh sách người dùng
-          </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          mb: 2,
+        }}
+      >
+        <Typography variant="h4" sx={{ mb: 3 }}>
+          Danh sách người dùng
+        </Typography>
+        {selectedUsers.length > 0 && (
+          <Button
+            variant="contained"
+            color="error"
+          >
+            Xoá sản phẩm
+          </Button>
+        )}
+        </Box>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }}>
+            <TableHead>
+              <TableRow>
+                {userData.user.role.name==="Admin"?
+                (<TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectAll}
+                    indeterminate={
+                      selectedUsers.length > 0 &&
+                      selectedUsers.length < users.length
+                    }
+                    onChange={handleSelectAllChange}
+                    color="primary"
+                  />
+                </TableCell>):
+                (<TableCell></TableCell>)}
+              
+                <TableCell sx={{ fontWeight: "bold" }}>
+                  Tên Khách Hàng
+                </TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Email</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Số điện thoại</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Địa chỉ</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Vai trò</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.users.map((user) => (
+                <TableRow
+                  key={user.id}
+                  onClick={() => handleRowClick(user)}
+                  style={{ cursor: "pointer" }}
+                >
+                  {userData.user.role.name==="Admin"?
+                  (<TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectedUsers.includes(user.id)}
+                    onClick={(e) => e.stopPropagation()}
+                    onChange={(e) => {
+                      handleCheckboxChange(user.id);
+                    }}
+                    
+                    color="primary"
+                  />
+                </TableCell>):
+                (<TableCell></TableCell>)}
+                
+                  <TableCell>{user.name}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                  <TableCell>{user.phone}</TableCell>
+                  <TableCell>{user.address}</TableCell>
+                  <TableCell>{user.role.name}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
 
-          <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 2 }}>
-            {selectedUserIds.length > 0 && (
-              <Button
-                variant="contained"
-                color="error"
-                startIcon={<DeleteIcon />}
-                onClick={handleDeleteSelectedUsers}
-              >
-                Xóa ({selectedUserIds.length}) người dùng
-              </Button>
+        {/* Modal for User Details */}
+        <Modal
+          open={openModal}
+          onClose={handleCloseModal}
+          aria-labelledby="modal-title"
+          aria-describedby="modal-description"
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: 500,
+              maxHeight: "80vh",
+              bgcolor: "background.paper",
+              border: "2px solid #000",
+              boxShadow: 24,
+              p: 4,
+              borderRadius: 2,
+              overflowY: "auto",
+            }}
+          >
+            {selectedUser && (
+              <>
+                <Typography
+                  id="modal-title"
+                  variant="h4"
+                  component="h2"
+                  sx={{ mb: 2 }}
+                >
+                  Chi Tiết người dùng
+                </Typography>
+                {isEditing ? (
+                  <>
+                    {/* Name */}
+                    <TextField
+                      label="Tên khách hàng"
+                      name="name"
+                      value={selectedUser.name}
+                      onChange={handleChange}
+                      fullWidth
+                      sx={{ mb: 2 }}
+                    />
+
+                    {/* Email */}
+                    <TextField
+                      label="Email"
+                      name="email"
+                      value={selectedUser.email}
+                      onChange={handleChange}
+                      fullWidth
+                      sx={{ mb: 2 }}
+                    />
+
+                    {/* Phone */}
+                    <TextField
+                      label="Số điện thoại"
+                      name="phone"
+                      value={selectedUser.phone}
+                      onChange={handleChange}
+                      fullWidth
+                      sx={{ mb: 2 }}
+                    />
+
+                    {/* Address */}
+                    <TextField
+                      label="Địa chỉ"
+                      name="address"
+                      value={selectedUser.address}
+                      onChange={handleChange}
+                      fullWidth
+                      sx={{ mb: 2 }}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <Typography>
+                      <strong>Tên:</strong> {selectedUser.name}
+                    </Typography>
+                    <Typography>
+                      <strong>Email:</strong> {selectedUser.email}
+                    </Typography>
+                    <Typography>
+                      <strong>Số điện thoại:</strong> {selectedUser.phone}
+                    </Typography>
+                    <Typography>
+                      <strong>Địa chỉ:</strong> {selectedUser.address}
+                    </Typography>
+                    <Typography>
+                      <strong>Vai trò:</strong> {selectedUser.role.name}
+                    </Typography>
+                  </>
+                )}
+                {userData.user.role.name==="Admin" || userId===selectedUser.id?
+                (<Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    p: 2,
+                    borderTop: "1px solid #ddd",
+                  }}
+                >
+                  
+                  <Button variant="contained" onClick={handleEditToggle}>
+                    <UpdateIcon />
+                    {isEditing ? "Lưu" : "Cập Nhật"}
+                  </Button>
+                </Box>):
+              (<></>)}
+                
+              </>
             )}
           </Box>
-
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }}>
-              <TableHead>
-                <TableRow>
-                  {userData.user.role.name === "Admin" ? (
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        checked={selectAll}
-                        indeterminate={
-                          selectedUsers.length > 0 &&
-                          selectedUsers.length < users.length
-                        }
-                        onChange={handleSelectAllChange}
-                        color="primary"
-                      />
-                    </TableCell>
-                  ) : (
-                    <TableCell></TableCell>
-                  )}
-
-                  <TableCell sx={{ fontWeight: "bold" }}>
-                    Tên Khách Hàng
-                  </TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>Email</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>
-                    Số điện thoại
-                  </TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>Địa chỉ</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>Vai trò</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.users.map((user) => (
-                  <TableRow
-                    key={user.id}
-                    onClick={() => handleRowClick(user)}
-                    style={{ cursor: "pointer" }}
-                  >
-                    {userData.user.role.name === "Admin" ? (
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          checked={selectedUsers.includes(user.id)}
-                          onClick={(e) => e.stopPropagation()}
-                          onChange={(e) => {
-                            handleCheckboxChange(user.id);
-                          }}
-                          color="primary"
-                        />
-                      </TableCell>
-                    ) : (
-                      <TableCell></TableCell>
-                    )}
-
-                    <TableCell>{user.name}</TableCell>
-                    <TableCell>{user.email}</TableCell>
-                    <TableCell>{user.phone}</TableCell>
-                    <TableCell>{user.address}</TableCell>
-                    <TableCell>{user.role.name}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-
-          {/* Modal for User Details */}
-          <Modal
-            open={openModal}
-            onClose={handleCloseModal}
-            aria-labelledby="modal-title"
-            aria-describedby="modal-description"
-          >
-            <Box
-              sx={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                width: 500,
-                maxHeight: "80vh",
-                bgcolor: "background.paper",
-                border: "2px solid #000",
-                boxShadow: 24,
-                p: 4,
-                borderRadius: 2,
-                overflowY: "auto",
-              }}
-            >
-              {selectedUser && (
-                <>
-                  <Typography
-                    id="modal-title"
-                    variant="h4"
-                    component="h2"
-                    sx={{ mb: 2 }}
-                  >
-                    Chi tiết người dùng
-                  </Typography>
-                  {isEditing ? (
-                    <>
-                      {/* Name */}
-                      <TextField
-                        label="Tên khách hàng"
-                        name="name"
-                        value={selectedUser.name}
-                        onChange={handleChange}
-                        fullWidth
-                        sx={{ mb: 2 }}
-                      />
-
-                      {/* Email */}
-                      <TextField
-                        label="Email"
-                        name="email"
-                        value={selectedUser.email}
-                        onChange={handleChange}
-                        fullWidth
-                        sx={{ mb: 2 }}
-                      />
-
-                      {/* Phone */}
-                      <TextField
-                        label="Số điện thoại"
-                        name="phone"
-                        value={selectedUser.phone}
-                        onChange={handleChange}
-                        fullWidth
-                        sx={{ mb: 2 }}
-                      />
-
-                      {/* Address */}
-                      <TextField
-                        label="Địa chỉ"
-                        name="address"
-                        value={selectedUser.address}
-                        onChange={handleChange}
-                        fullWidth
-                        sx={{ mb: 2 }}
-                      />
-                    </>
-                  ) : (
-                    <>
-                      <Typography>
-                        <strong>Tên:</strong> {selectedUser.name}
-                      </Typography>
-                      <Typography>
-                        <strong>Email:</strong> {selectedUser.email}
-                      </Typography>
-                      <Typography>
-                        <strong>Số điện thoại:</strong> {selectedUser.phone}
-                      </Typography>
-                      <Typography>
-                        <strong>Địa chỉ:</strong> {selectedUser.address}
-                      </Typography>
-                      <Typography>
-                        <strong>Vai trò:</strong> {selectedUser.role.name}
-                      </Typography>
-                    </>
-                  )}
-                  {userData.user.role.name === "Admin" ||
-                  userId === selectedUser.id ? (
-                    <Box
-                      sx={{
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        p: 2,
-                        borderTop: "1px solid #ddd",
-                        overflowY: "auto",
-                      }}
-                    >
-                      <Button variant="contained" onClick={handleEditToggle}>
-                        <UpdateIcon />
-                        {isEditing ? "Lưu" : "Cập Nhật"}
-                      </Button>
-                    </Box>
-                  ) : (
-                    <></>
-                  )}
-                </>
-              )}
-            </Box>
-          </Modal>
-        </Box>
+        </Modal>
       </Box>
     </>
   );
